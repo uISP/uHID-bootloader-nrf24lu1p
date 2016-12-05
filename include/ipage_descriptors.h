b5efbd71c381c0671a78ec8a5598154a526dd815
Andrew Andrianov <andrew@ncrmnt.org>
2016-12-05 20:21:28 +0300

    Add ipage_descr.h
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

---
+++ include/ipage_descriptors.h
@@ -0,0 +1,16 @@
+#define IPAGE_OFF_string_zero 256
+#define IPAGE_LEN_string_zero 4
+#define IPAGE_OFF_g_usb_string_desc_1 260
+#define IPAGE_LEN_g_usb_string_desc_1 30
+#define IPAGE_OFF_g_usb_string_desc_2 290
+#define IPAGE_LEN_g_usb_string_desc_2 10
+#define IPAGE_OFF_g_usb_string_desc_3 300
+#define IPAGE_LEN_g_usb_string_desc_3 30
+#define IPAGE_OFF_g_usb_conf_desc 330
+#define IPAGE_LEN_g_usb_conf_desc 34
+#define IPAGE_OFF_g_usb_hid_report_desc 364
+#define IPAGE_LEN_g_usb_hid_report_desc 42
+#define IPAGE_OFF_g_usb_dev_desc 406
+#define IPAGE_LEN_g_usb_dev_desc 18
+#define IPAGE_OFF_ldr_dev_info 424
+#define IPAGE_LEN_ldr_dev_info 60
